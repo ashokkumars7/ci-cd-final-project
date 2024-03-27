8c7b746dc49ccf09a2b228233b02ea32cc055621
Ashokkumar Selvam <ashokkumar.s@terrapay.com>
2024-03-27 03:57:04 -0400

    Tasks added

--- service/routes.py
+++ service/routes.py
@@ -40,8 +40,10 @@ def list_counters():
     """Lists all counters"""
     app.logger.info("Request to list all counters...")
 
-    counters = [dict(name=count[0],
-        counter=count[1]) for count in COUNTER.items()]
+    counters = [
+        dict(name=count[0],
+        counter=count[1]) for count in COUNTER.items()
+    ]
 
     return jsonify(counters)
 
@@ -56,7 +58,7 @@ def create_counters(name):
 
     if name in COUNTER:
         return abort(
-            status.HTTP_409_CONFLICT, 
+            status.HTTP_409_CONFLICT,
             f"Counter {name} already exists"
         )
 
@@ -80,7 +82,7 @@ def read_counters(name):
 
     if name not in COUNTER:
         return abort(
-            status.HTTP_404_NOT_FOUND, 
+            status.HTTP_404_NOT_FOUND,
             f"Counter {name} does not exist"
         )
 
