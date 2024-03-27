15ca563ba04a808730a8d31588cf81626affd695
Ashokkumar Selvam <ashokkumar.s@terrapay.com>
2024-03-27 05:03:05 -0400

    lint fixes

--- service/routes.py
+++ service/routes.py
@@ -41,8 +41,9 @@ def list_counters():
     app.logger.info("Request to list all counters...")
 
     counters = [
-        dict(name=count[0],
-        counter=count[1]) for count in COUNTER.items()]
+        dict(name=count[0], counter=count[1]) for count in COUNTER.items()
+    ]
+
 
     return jsonify(counters)
 
@@ -98,7 +99,10 @@ def update_counters(name):
     app.logger.info("Request to Update counter: %s...", name)
 
     if name not in COUNTER:
-        return abort(status.HTTP_404_NOT_FOUND, f"Counter {name} does not exist")
+        return abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Counter {name} does not exist"
+        )
 
     COUNTER[name] += 1
 
